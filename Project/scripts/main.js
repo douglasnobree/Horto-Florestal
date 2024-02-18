51fb883ecf0f2fee3dce027c6963ca983f4e5fc5
Alessandro Cauã <arquerobranco9990@gmail.com>
2024-02-18 20:36:36 -0300

    Site modificado

---
+++ Project/scripts/main.js
@@ -0,0 +1,13 @@
+
+var menuItem = docurment.querySelectorAll('.item-menu')
+
+function selectLink(){
+    menuItem.forEach((item)=>
+        item.classList.remove('ativo')
+    )
+    this.classList.add('ativo')
+}
+
+menuItem.forEach((item)=>
+    item.addEventListener('click', selectLink) //não pode ter '' para n ser chamado primeiro
+)
