e6bf185e6b81b85ede1bbac0afa99fbae0d35862
Douglas <douglasclaudino543@gmail.com>
2024-02-19 00:44:48 -0300

    bug selected

--- Project/scripts/main.js
+++ Project/scripts/main.js
@@ -1,13 +1,17 @@
-//TEM QUE ARRUMAR ISSO AQUI
-function selectLink(){
-    menuItem.forEach((item)=>
-        item.classList.remove('ativo') //Remove a classe ativo de todos os itens
-    )
-    this.classList.add('ativo') // Adiciona a classe ativo ao item clicado
-}
+document.addEventListener('DOMContentLoaded', function () {
+    var menuItem = document.querySelectorAll('.item-menu');
+    console.log(menuItem);
 
-menuItem.forEach((item)=>
-    item.addEventListener('click', selectLink) //não pode ter '' para n ser chamado primeiro
-)
+    const selectLink = (event) => {
+        menuItem.forEach((item) =>
+            item.classList.remove('ativo') // Remove a classe ativo de todos os itens
+            
+        );
+        console.log(event.currentTarget);
+        event.currentTarget.classList.add('ativo'); // Adiciona a classe ativo ao item clicado
+    };
 
-document.getElementsByClassName()
+    menuItem.forEach((item) =>
+        item.addEventListener('mousedown', selectLink) // Agora usa uma função de flecha para manter o contexto correto
+    );
+});
