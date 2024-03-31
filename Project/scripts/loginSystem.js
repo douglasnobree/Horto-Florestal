0ee421f94ad47be7f171b8258e4d8e701e096fb9
Douglas <douglasclaudino543@gmail.com>
2024-03-31 03:37:11 -0300

    Fix formatting and remove unnecessary code

--- Project/scripts/loginSystem.js
+++ Project/scripts/loginSystem.js
@@ -7,13 +7,16 @@ document.addEventListener('DOMContentLoaded', async () => {
         const username = formData.get('username');
         const password = formData.get('password');
 
-        const response = await fetch('https://horto-florestal-backend.onrender.com/plants/login', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({ username, password }),
-        });
+        const response = await fetch(
+            'https://horto-florestal-backend.onrender.com/plants/login',
+            {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ username, password }),
+            }
+        );
         const data = await response.json();
         console.log(data);
         const token = data.token;
