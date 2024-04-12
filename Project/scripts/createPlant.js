64352f113411b733a259993c9ec2e603e80b21f8
Douglas <douglasclaudino543@gmail.com>
2024-04-12 18:55:56 -0300

    Fixing Create Page

--- Project/scripts/createPlant.js
+++ Project/scripts/createPlant.js
@@ -5,18 +5,28 @@ document.addEventListener('DOMContentLoaded', async () => {
         event.preventDefault();
         const formData = new FormData(form);
         const nome = formData.get('plantName');
-        const especie = formData.get('platNameAlternative');
+        const especie = formData.get('plantSpecies');
         const descricao = formData.get('plantDescription');
-        const img_url = formData.get('plantUrl');
+        const img_url = formData.get('img_url');
+        const localizacao = formData.get('localizacao');
+        const floracao = formData.get('floracao');
+        const curiosidades = formData.get('curiosidades');
+        const cuidados = formData.get('cuidados');
+        const rega = formData.get('rega');
 
         const plant = {
             nome,
             especie,
             descricao,
             img_url,
+            localizacao,
+            floracao,
+            curiosidades,
+            cuidados,
+            rega,
         };
         const response = await fetch(
-            'https://backend.cactustheca.shop/plants/createNewPlant',
+            'http://localhost:3333/plants/createNewPlant',
             {
                 method: 'POST',
                 headers: {
